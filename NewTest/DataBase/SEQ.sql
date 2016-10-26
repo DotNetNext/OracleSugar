d1d10633c9eb3500dcb5ec854c88d3f79bba150d
sunkaixuan <610262374@qq.com>
2016-10-27 04:28:34 +0800

    ok

--- NewTest/DataBase/SEQ.sql
+++ NewTest/DataBase/SEQ.sql
@@ -1,15 +1,25 @@
-﻿USE [master]
-
-
-CREATE SEQUENCE SEQ
+﻿ 
+CREATE SEQUENCE SEQ;
+Alter Sequence SEQ Increment By 1000;
+Select SEQ.NextVal From Dual;
+Alter Sequence SEQ Increment By 1;
  
 
-CREATE SEQUENCE SEQ2
+CREATE SEQUENCE SEQ2;
+Alter Sequence SEQ2 Increment By 1000;
+Select SEQ2.NextVal From Dual;
+Alter Sequence SEQ2 Increment By 1;
  
 
-CREATE SEQUENCE SEQ3
+CREATE SEQUENCE SEQ3;
+Alter Sequence SEQ3 Increment By 1000;
+Select SEQ3.NextVal From Dual;
+Alter Sequence SEQ3 Increment By 1;
  
 
-CREATE SEQUENCE SEQ4
+CREATE SEQUENCE SEQ4;
+Alter Sequence SEQ4 Increment By 1000;
+Select SEQ4.NextVal From Dual;
+Alter Sequence SEQ4 Increment By 1;
  
  
